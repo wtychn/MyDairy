c0ca9ffececbc3387190a9237dc5555eedfb015a
wtychn <452443579@qq.com>
2021-06-18 12:44:33 +0800

    feat(DairyService): add CRUD

--- src/main/java/com/wtychn/mydairy/service/DairyService.java
+++ src/main/java/com/wtychn/mydairy/service/DairyService.java
@@ -18,22 +18,34 @@ public class DairyService {
     @Autowired
     DairyDAO dairyDAO;
 
-    List<Dairy> userDairies;
 
     public List<Dairy> listByUser(User user) {
         // 按日期倒序及当日时间正序排列
         List<Sort.Order> orders = new ArrayList<>();
         orders.add(new Sort.Order(Sort.Direction.DESC, "time"));
         orders.add(new Sort.Order(Sort.Direction.ASC, "tid"));
-        userDairies = dairyDAO.findByUser(user, Sort.by(orders));
-        return userDairies;
+        return dairyDAO.findByUser(user, Sort.by(orders));
     }
 
-    public List<Dairy> listByTime(Date time) {
-        return userDairies.stream()
+    public List<Dairy> listByTime(Date time, User user) {
+        return listByUser(user).stream()
                 .filter(dairy -> dairy.getTime().equals(time))
                 .collect(Collectors.toList());
     }
 
-    // TODO: Dairy CRUD
+    public Dairy get(int id) {
+        return dairyDAO.getById(id);
+    }
+
+    public void delete(Dairy bean) {
+        dairyDAO.delete(bean);
+    }
+
+    public void set(Dairy bean) {
+        dairyDAO.save(bean);
+    }
+
+    public void add(Dairy bean) {
+        dairyDAO.save(bean);
+    }
 }
